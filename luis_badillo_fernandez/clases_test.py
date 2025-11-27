b155234f8ab0be90a8316a9a2171cfab26591232
Jesus <pachecobarrios7@gmail.com>
2025-11-27 11:07:21 -0500

    test unit corregido

--- luis_badillo_fernandez/clases_test.py
+++ luis_badillo_fernandez/clases_test.py
@@ -10,15 +10,32 @@ class Carrito:
     def add(self, producto):
         if isinstance(producto, Producto):
             self.productos.append(producto)
+    
+    def cantidad(self):
+        return len(self.productos)
         
 
 class Motor:
     def __init__(self, potencia):
         self.potencia = potencia
+    
+    def encender(self):
+        self.encendido = True
+        return "motor encendido"
+            
         
 class Carro:
-    def __init__(self, motor_potencia):
-        self.motor = Motor(motor_potencia)
+    def __init__(self, motor, pintura):
+        self.motor = motor
+        self.pintura = pintura
+    
+    def arrancar(self):
+        
+        return self.motor.encender()
+
+    
+        
+        
         
 
                 
--- luis_badillo_fernandez/test_unit_test.py
+++ luis_badillo_fernandez/test_unit_test.py
@@ -2,38 +2,45 @@ from clases_test import Producto, Carrito, Carro, Motor
 
 def test_intancia_ag():
     carrito = Carrito()
+    assert isinstance(carrito,Carrito)
     
-    producto1 = Producto("mango", 5000)
-    carrito.add(producto1)
-    
-    assert isinstance(carrito.productos[0], Producto)
-    assert isinstance(carrito, Carrito)
-
 def test_metodos_ag():
     carrito = Carrito()
     
     producto1 = Producto("pera", 3000)
+    producto2 = Producto("manzana", 2500)
+    assert len(carrito.productos) == 0
     carrito.add(producto1)
+    carrito.add(producto2)
+    assert len(carrito.productos) == 2
+    assert carrito.cantidad() == 2
     
-    assert hasattr(carrito, "add")
-
+    
 def test_atributos_ag():
-    carrito = Carrito()
-    
-    producto1 = Producto("papa", 1200)
-    
-    assert hasattr(producto1, "nombre")
-    assert producto1.precio == 1200
+    producto1 = Producto("leche", 4500)
+    assert producto1.nombre == "leche"
+    assert producto1.precio == 4500
     
 
 def test_instancia_comp():
-    carro = Carro(2000)
-    assert isinstance(carro.motor, Motor)
+    motor = Motor(1200)
+    carro = Carro(motor, "rojo")
+    
+    assert isinstance(motor, Motor)
+    assert isinstance(carro, Carro)
+    
 
 def test_metodos_comp():
-    carro = Carro(1000)
-    assert hasattr(Carro, "__init__")
+    motor = Motor(1200)
+    carro = Carro(motor, "rojo")
+    
+    encendido = carro.arrancar()
+    
+    assert "motor encendido" in encendido
 
 def test_atributos_comp():
-    carro = Carro(2500)
-    assert carro.motor.potencia == 2500
+    motor = Motor(1200)
+    carro = Carro(motor, "rojo")
+    
+    assert motor.potencia == 1200
+    assert carro.pintura == "rojo"
